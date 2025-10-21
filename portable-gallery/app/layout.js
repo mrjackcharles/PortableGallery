329b842ab26383992658f14510180a53fd1a9d5a
Jack Charles <jackcharles@MacBookAir.lan>
2025-10-21 14:26:59 -0400

    removed network fetched fonts to just local

--- portable-gallery/app/layout.js
+++ portable-gallery/app/layout.js
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 const googleSansCode = localFont({
     src: [
@@ -36,7 +25,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} ${googleSansCode.variable} ${googleSansCode.className} antialiased`}
+                className={`${googleSansCode.variable} ${googleSansCode.className} antialiased`}
             >
                 {children}
             </body>
